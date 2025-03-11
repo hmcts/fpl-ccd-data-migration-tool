8c3674d64f3f31893a68f21e2d6f01a4cf05d629
Chak Shing, Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2025-03-11 10:28:33 +0000

    Update DataMigrationServiceImpl.java

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -19,6 +19,7 @@ import uk.gov.hmcts.reform.migration.query.MustNot;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +30,7 @@ import java.util.function.Predicate;
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 import static org.apache.commons.lang3.ObjectUtils.isEmpty;
+import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 
 @Slf4j
 @Component
@@ -41,11 +43,13 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
         "DFPL-2572", this::triggerTtlMigration,
         "DFPL-2635", this::triggerOnlyMigration,
         "DFPL-2642", this::triggerOnlyMigration,
+        "DFPL-2423", this::triggerOnlyMigration,
         "DFPL-2640", this::triggerOnlyMigration
         );
 
     private final Map<String, EsQuery> queries = Map.of(
         "DFPL-2585", this.closedCases(),
+        "DFPL-2423", this.topLevelFieldExistsQuery("proceeding"),
         "DFPL-2572", this.openCases()
     );
 
