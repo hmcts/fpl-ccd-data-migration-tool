41c97ed9371a6b65a9ae95abbc656625c9b1d90e
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2025-05-21 16:47:52 +0100

    DFPL-2360: Add migration function (#289)

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -46,7 +46,7 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
         "DFPL-2572", this::triggerTtlMigration,
         "DFPL-2739", this::triggerOnlyMigration,
         "DFPL-2733", this::triggerOnlyMigration,
-        "DFPL-2756", this::triggerOnlyMigration
+        "DFPL-2360", this::triggerOnlyMigration
         );
 
     private final Map<String, EsQuery> queries = Map.of(
