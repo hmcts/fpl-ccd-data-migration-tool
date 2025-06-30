eef8d17e11d74130e668982c14647eadc19315f8
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2025-06-30 15:17:18 +0100

    query cases of specified group

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.domain.exception.CaseMigrationSkippedException;
@@ -30,11 +31,14 @@ import java.util.function.Predicate;
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 import static org.apache.commons.lang3.ObjectUtils.isEmpty;
+import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class DataMigrationServiceImpl implements DataMigrationService<Map<String, Object>> {
+    @Value("${case-migration.case_group}")
+    String caseGroupString;
 
     public static final String COURT = "court";
     private final Map<String, Function<CaseDetails, Map<String, Object>>> migrations = Map.of(
@@ -42,7 +46,8 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
         );
 
     private final Map<String, EsQuery> queries = Map.of(
-        "DFPL-test", this.openCases()
+        "DFPL-test", this.openCases(),
+        "DFPL-log", this.allNonDeletedCases()
     );
 
     private EsQuery allNonDeletedCases() {
@@ -97,6 +102,11 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
 
     @Override
     public EsQuery getQuery(String migrationId) {
+        // Temp for migration tool perf testing
+        if ("DFPL-log".equals(migrationId) && isNotEmpty(caseGroupString)) {
+            return this.topLevelFieldExistsQuery("familyManCaseNumber", caseGroupString);
+        }
+
         if (!queries.containsKey(migrationId)) {
             throw new NoSuchElementException("No migration mapped to " + migrationId);
         }
@@ -137,6 +147,10 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
                     .build()))
                 .build())
             .build();
+    }
+
+    private EsQuery topLevelFieldExistsQuery(String field, String values) {
+        return MatchQuery.of("data." + field, values);
     }
 
     private Map<String, Object> triggerOnlyMigration(CaseDetails caseDetails) {
