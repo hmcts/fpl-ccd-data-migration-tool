570d2e4590e19d29c945577c687afa79b148903d
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2025-07-04 11:07:17 +0100

    Rollback DFPL-2797 (#320)
    * Revert "Dfpl 2797 sort by familyManCaseNumber (#317)"
    
    This reverts commit 702d86909df9ba0a92d7562a5e98d9d49962579b.
    
    * Revert "DFPL 2797  (#315)"
    
    This reverts commit 987280ac20e8c4cafe8bc0272263f01106576792.

--- src/main/java/uk/gov/hmcts/reform/migration/repository/ElasticSearchRepository.java
+++ src/main/java/uk/gov/hmcts/reform/migration/repository/ElasticSearchRepository.java
@@ -25,7 +25,7 @@ public class ElasticSearchRepository {
 
     public static final Sort SORT_BY_REF = Sort.builder()
         .clauses(List.of(
-            SortQuery.of("data.familyManCaseNumber.keyword", SortOrder.ASC) // Temp for migration tool perf testing
+            SortQuery.of("reference.keyword", SortOrder.DESC)
         ))
         .build();
 
@@ -44,7 +44,7 @@ public class ElasticSearchRepository {
     }
 
     @SneakyThrows
-    public List<CaseDetails> search(String userToken, String caseType, EsQuery query, int size, Integer after) {
+    public List<CaseDetails> search(String userToken, String caseType, EsQuery query, int size, String after) {
         requireNonNull(query);
         SearchResult result = null;
 
