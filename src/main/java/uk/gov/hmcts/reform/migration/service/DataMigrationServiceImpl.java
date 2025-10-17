cfe2690460434e6e786a3e83a683a553eb8b1f28
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2025-10-17 14:25:42 +0100

    add DFPL-2914 migration (#331)

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -40,7 +40,8 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
     private final Map<String, Function<CaseDetails, Map<String, Object>>> migrations = Map.of(
         "DFPL-log", this::triggerOnlyMigration,
         "DFPL-2805", this::triggerOnlyMigration,
-        "DFPL-2818", this::triggerOnlyMigration
+        "DFPL-2818", this::triggerOnlyMigration,
+        "DFPL-2914", this::triggerOnlyMigration
         );
 
     private final Map<String, EsQuery> queries = Map.of(
