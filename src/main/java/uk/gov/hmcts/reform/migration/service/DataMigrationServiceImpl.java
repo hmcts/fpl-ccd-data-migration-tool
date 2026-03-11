af7d19e4a514603f43ff003813dca2ab38d2b88f
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2026-03-11 09:56:54 +0000

    Update DataMigrationServiceImpl.java (#366)

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -59,7 +59,8 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
         "DFPL-log", this::triggerOnlyMigration,
         "DFPL-2773", this::triggerOnlyMigration,
         "DFPL-2773-rollback", this::triggerOnlyMigration,
-        "DFPL-3080", this::triggerOnlyMigration
+        "DFPL-3080", this::triggerOnlyMigration,
+        "DFPL-3085", this::triggerOnlyMigration
     );
 
     private final Map<String, EsQuery> queries = Map.of(
