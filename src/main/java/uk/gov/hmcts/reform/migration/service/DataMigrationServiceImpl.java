52d5f130a0b79f05724013c835f48c615f8cc861
alistair.osborne <alistair.osborne@justice.gov.uk>
2026-04-22 12:41:27 +0100

    DFPL-3191: Migration to remove confidential details

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -60,7 +60,7 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
         "DFPL-2773", this::triggerOnlyMigration,
         "DFPL-2773-rollback", this::triggerOnlyMigration,
         "DFPL-3080", this::triggerOnlyMigration,
-        "DFPL-3085", this::triggerOnlyMigration,
+        "DFPL-3191", this::triggerOnlyMigration,
         "DFPL-3101", this::triggerOnlyMigration
     );
 
