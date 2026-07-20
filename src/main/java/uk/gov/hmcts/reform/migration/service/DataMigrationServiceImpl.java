dd6913e42158dcbeb1eb2732e5a3f33c715a4976
alistair.osborne <alistair.osborne@justice.gov.uk>
2026-07-20 14:46:02 +0100

    DFPL-3306: Grant case access to J A Hughes SOLICITORC

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -57,7 +57,7 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
     public static final String COURT = "court";
     private final Map<String, Function<CaseDetails, Map<String, Object>>> migrations = Map.of(
         "DFPL-log", this::triggerOnlyMigration,
-        "DFPL-3290", this::triggerOnlyMigration,
+        "DFPL-3306", this::triggerOnlyMigration,
         "DFPL-3296", this::triggerOnlyMigration
     );
 
